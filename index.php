a0d283459bfd6dcf1a6ead203fde4dfccf29a358
GASADER <kajorhkaen@gmail.com>
2022-07-29 02:54:57 +0700

    first connect

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+<?php include('serve.php');?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <p1>hallow</p1>
 </body>
 </html>

3bfada9177c04f8b480123373cd508116f0cee7b
GASADER <kajorhkaen@gmail.com>
2022-07-29 05:02:24 +0700

    homepage back register

--- login.php
+++ login.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RegisterPage</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="header">
-        <h2>Register</h2>
+<div class="header">
+        <h2>Login</h2>
     </div>
     <form action="register_db.php">
         <div class="input-group">
@@ -18,21 +18,12 @@
             <input type="text" name="username">
         </div>
         <div class="input-group">
-            <label for="">Email</label>
-            <input type="email" name="email">
-        </div>
-        <div class="input-group">
             <label for="">Password</label>
-            <input type="password" name="password1">
+            <input type="password" name="password">
         </div>
         <div class="input-group">
-            <label for="">Confirm Password</label>
-            <input type="password" name="password2">
+            <button type="submit" name="login_user" class="btn">Login</button>
         </div>
-        <div class="input-group">
-            <button type="submit" name="reg_user" class="btn">Register</button>
-        </div>
-        <p>Already a member ?<a href="login.php">Sing in</a></p>
-    </form>
+        <p>Not yet a member ?<a href="register.php">Sing up</a></p>
 </body>
 </html>
